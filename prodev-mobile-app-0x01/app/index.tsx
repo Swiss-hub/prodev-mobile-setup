63b04af9b5da8d2a75765e91c06f959b5ef2e1fe
Ilechukwu Francis C <ilechukwufran6x@gmail.com>
2025-10-06 04:08:01 +0100

    Correction

--- prodev-mobile-app-0x01/app/index.tsx
+++ prodev-mobile-app-0x01/app/index.tsx
@@ -1,13 +1,16 @@
 import { StyleSheet, Text, View } from "react-native";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function Index() {
   return (
-    <View style={styles.container} >
-      <Text>Entry Screen - Awesome</Text>
+    <SafeAreaProvider>
+      <View style={styles.container} >
+        <Text>Entry Screen - Awesome</Text>
+      </View>
       <Text style={styles.largeText}>Typescript is great if you practice more</Text>
       <Text style={styles.largeText}>React Native provides you a single codebase for cross platforms</Text>
       <Text style={styles.largeText}>ALX is awesome</Text>
-    </View>
+    </SafeAreaProvider>
   );
 }
 
